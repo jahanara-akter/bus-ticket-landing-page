735b236c8e9bfd3a970979a6810ba08938594507
Jahanara Akter <jahanaraaktersuma100@gmail.com>
2024-02-19 19:34:45 +0600

    responsive

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener(function(){
+    const tikcetPrice = 550;
+    const maxTikcets = 4;
+    let selectedTickets = 0;
+
+    const ticketButtons = document.querySelectorAll(".price");
+    const totalPriceElement = document.querySelectorAll(".grid-cols-3:last-child")
+})
